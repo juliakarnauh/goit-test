77331aaee3387aa79760970fb31a04cf04a7ce6e
juliakarnaukh <juliakarnayh04@gmail.com>
2023-05-21 00:32:30 +0300

    update App

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename="/goit-test">
         <App />
       </BrowserRouter>
     </React.StrictMode>
