22dbc82292d593f2ddf4d948c45a8b793381a2cb
juliakarnaukh <juliakarnayh04@gmail.com>
2023-05-21 01:59:42 +0300

    fix deploy

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="tweets" element={<TweetsPage />} />
+        <Route path="*" element={<HomePage />} />
       </Route>
-      <Route path="*" element={<HomePage />} />
     </Routes>
   );
 };
